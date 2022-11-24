98cb77676b3d864ed27ee87a295704e697eb5bc0
rayhack <ismaraymond@gmail.com>
2022-11-24 21:35:54 +0000

    suppression du commentaire dans chat_page

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -12,49 +12,4 @@ class ChatPage extends StatelessWidget {
       }),
     );
   }
-
-  /*
-  return SingleChildScrollView(
-      child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 12),
-                  child: Row(
-                    children: [
-                      ClipRRect(
-                        child: Image.asset(
-                          "name",
-                          width: 65,
-                          height: 65,
-                        ),
-                      ),
-                      Padding(
-                          padding: EdgeInsets.only(left: 20),
-                          child: Column(
-                            children: [
-                              Text(
-                                "Raymond",
-                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 8,),
-                              Text(
-                                "hello, comment ca va?",
-                                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500, color: Colors.black54),
-                              ),
-                            ],
-                          ),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-      ),
-    );
-   */
 }
